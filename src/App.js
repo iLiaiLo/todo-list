66c3654445d05725f111a37c5bca5df6c658dfde
Ilo Tech <ilo.nikatsadze@gmail.com>
2024-03-06 20:57:23 +0400

    some new code added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+  const [text,setText]=React.useState("");
+  const [content,setContent]=React.useState([]);
+
+  React.useEffect(()=>{
+    const storagedata=localStorage.getItem("todoData");
+    if(storagedata){
+      setContent(JSON.parse(storagedata));
+    }
+  },[])
+  
+  React.useEffect(()=>{
+    localStorage.setItem("todoData",JSON.stringify(content))
+  },[content])
+  
+  function changeText(e){
+    setText(e.target.value);
+  }
+  
+  function addContent(){
+    setContent(prewcontent=>[{id:Math.random(),Text:text,completed:false},...prewcontent])
+  
+  }
+
+  function checkId(id){
+    setContent(content.map((item)=>{
+      if(item.id===id){
+        item.completed=!item.completed
+      }
+      return item
+    }))
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="addButTask">
+    <input className="addtask" placeholder="create plan" type="text" value={text} onChange={changeText} />
+    <button className="addBut" onClick={addContent}>add content</button>
+    
     </div>
+    <div>
+
+    {content.map(item=>{
+         return (
+          <div style={{marginTop:"20px",display:"flex",flexDirection:"row",gap:"15px"}}>
+          <input type="checkbox" checked={item.completed} style={{width:"5vh"}} onChange={()=>checkId(item.id)} />
+        <div className="task" key={item.id}>{item.Text}</div>
+        <button className="removeBut" onClick={()=>setContent(content.filter(it=>it.id!==item.id))}>remove</button>
+        </div>
+        )
+      })}
+
+    </div>
+    
+  </div>   
+
   );
 }
 
